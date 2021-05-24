2d3a74a0a84c763e8e6d403c5a3ff1270351cd65
mahir88 <mahir.88@bk.ru>
2021-05-24 15:18:10 +0500

    salam

---
+++ txt.php
@@ -0,0 +1,12 @@
+<?php
+
+echo 'salam';
+
+// cd .git // yaranmiw gizli papkaya baxmaq ucun
+// cd .. // ust papkaya kecmak ucun istifade olunur
+
+// git status
+
+// git add txt.php // untracked fayli tracked etmak ucun
+// git add. // butun untracked fayllari tracked etmak ucun
+// git rm --cached txt.php // trackedi untracked etmak ucun
